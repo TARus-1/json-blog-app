0b72581f56eb3fea1a21e3cbc0a1e8edd6da7152
TARus-1 <trusse25@charlotte.edu>
2026-04-28 21:33:49 -0400

    added page that checks for login.

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export function AuthProvider({children}){
+    const [user, setUser] = useState(null);
+    
+    function login(username){
+        setUser({username});
+    }
+
+    function logout() {
+        setUser(null);
+    }
+
+    return( 
+        <AuthContext.Provider value={{user, login, logout}}>
+            
+        </AuthContext.Provider>
+    );
+}
