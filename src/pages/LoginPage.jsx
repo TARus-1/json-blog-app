f9d0fcdd803cb9f5eabc33af69395ae3017099fc
TARus-1 <trusse25@charlotte.edu>
2026-04-28 21:47:47 -0400

    added functions to login page. addeed some default text there/

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -7,4 +7,24 @@ export default function LoginPage() {
     const[username, setUsername] = useState("");
     const {login} = useAuth();
     const navigate = useNavigate();
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        if (username.trim() === "") {
+         return;
+        }
+
+        login(username);
+        navigate("/");
+    }
+
+    return (
+        <main>
+            <div>
+                <h2>Login</h2>
+                <p>You must log in to add comments!</p>
+            </div>
+        </main>
+    )
 }
