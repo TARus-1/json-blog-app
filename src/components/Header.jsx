62518c36e1825a668bb4017946f728aab041eb2d
TARus-1 <trusse25@charlotte.edu>
2026-04-28 22:09:46 -0400

    added username consitency and check on the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,30 @@
 import {Link} from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 export default function Header(){
+
+    const { user, logout } = useAuth();
     return (
         <nav className="navbar">
-            <h1>Lab 8 Blog</h1>
+            <h1>Project 2 Blog</h1>
             <div className="nav-links">
                 <Link to="/"><h2>Home</h2></Link>
                 <h2>About</h2>
                 <Link to="/contact"><h2>Contact</h2></Link>
+            
+                {user ? (
+          <>
+            <span className="username">Hey, {user.username}</span>
+            <button onClick={logout}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <Link to="/login">
+            Login
+          </Link>
+        )}
+
             </div>
             
         </nav>
